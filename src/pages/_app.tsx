28930c90a866cc95a63d777cb2a5fbf217e49888
nakanoyuki <yukich41@gmail.com>
2024-05-30 21:21:09 +0900

    👍: ThemeProvider設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from "next/app";
 import Layout from "./components/layout/Layout";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@/theme/theme";
+import { CssBaseline } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
