2659f4725b40586062c7ef2f67b819b3a231672c
nakanoyuki <yukich41@gmail.com>
2024-06-10 20:37:37 +0900

    ✨: theme.tsにカスタムカラー追記

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,38 @@
-import { createTheme } from "@mui/material";
+import { PaletteColor, PaletteColorOptions, createTheme } from "@mui/material";
+import { blue, green, red } from "@mui/material/colors";
+
+declare module "@mui/material/styles" {
+  interface palette {
+    incomeColor: PaletteColor;
+    expenseColor: PaletteColor;
+    balanceColor: PaletteColor;
+  }
+  interface PaletteOptions {
+    incomeColor?: PaletteColorOptions;
+    expenseColor?: PaletteColorOptions;
+    balanceColor?: PaletteColorOptions;
+  }
+}
 
 export const theme = createTheme({
   typography: {
     fontFamily: 'Noto Sans JP, Roboto, "Helvetica Neue", Arial, sans-serif',
+  },
+  palette: {
+    incomeColor: {
+      main: blue[500],
+      light: blue[100],
+      dark: blue[700],
+    },
+    expenseColor: {
+      main: red[500],
+      light: red[100],
+      dark: red[700],
+    },
+    balanceColor: {
+      main: green[500],
+      light: green[100],
+      dark: green[700],
+    },
   },
 });
