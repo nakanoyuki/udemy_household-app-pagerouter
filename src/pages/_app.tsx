b7473f2521f73e18c338aeab58a40cd4ac44ef38
nakanoyuki <yukich41@gmail.com>
2024-06-18 19:50:42 +0900

    🔥: 使用していないimport 削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import Layout from "./components/layout/Layout";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "@/theme/theme";
-import { CssBaseline } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
