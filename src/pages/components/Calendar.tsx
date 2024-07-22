5fb93a93b84844df5a15624c176c4faf3bc0f05b
nakanoyuki <yukich41@gmail.com>
2024-07-22 20:45:05 +0900

    ✨: カレンダーに収支を表示

--- src/pages/components/Calendar.tsx
+++ src/pages/components/Calendar.tsx
@@ -5,8 +5,8 @@ import React, { useState } from "react";
 import { EventChangeArg } from "@fullcalendar/core";
 import styles from "../../Calendar.module.scss";
 import { calculateDailyBalances } from "@/utils/financeCalculation";
-import { Transaction } from "@/type";
-import { formatMonth } from "@/utils/formatMonth";
+import { Balance, CalenderContent, Transaction } from "@/type";
+import { format } from "date-fns";
 
 const events = [
   { title: "Meeting", start: "2024-06-11" },
@@ -19,48 +19,61 @@ const events = [
   },
 ];
 
-interface Props {
-  monthlyTransactions: Transaction[];
-}
-
-const [transactions, setTransactions] = useState<Transaction[]>([]);
-const [currentMonth, setCurrentMonth] = useState(new Date());
-
-const monthlyTransactions = transactions.filter((transaction) =>
-  transaction.date.startsWith(formatMonth(currentMonth))
-);
-
-const dailyBalances = calculateDailyBalances(monthlyTransactions);
-
-const renderEventCount = (eventInfo: EventChangeArg) => {
-  return (
-    <>
-      <div
-        className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventIncome}`}
-      >
-        {eventInfo.event.extendedProps.income}
-      </div>
-      <div
-        className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventExpense}`}
-      >
-        {eventInfo.event.extendedProps.expense}
-      </div>
-      <div
-        className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventBalance}`}
-      >
-        {eventInfo.event.extendedProps.balance}
-      </div>
-    </>
-  );
-};
-
 const Calendar = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const monthlyTransactions = transactions.filter((transaction) =>
+    transaction.date.startsWith(format(currentMonth, "yyyy-MM"))
+  );
+
+  const dailyBalances = calculateDailyBalances(monthlyTransactions);
+
+  const createCalenderEvents = (
+    dailyBalances: Record<string, Balance>
+  ): CalenderContent[] => {
+    return Object.keys(dailyBalances).map((date) => {
+      const { income, expense, balance } = dailyBalances[date];
+
+      return {
+        start: date,
+        income: 300,
+        expense: 200,
+        balance: 100,
+      };
+    });
+  };
+
+  const calenderEvents = createCalenderEvents(dailyBalances);
+
+  const renderEventCount = (eventInfo: EventChangeArg) => {
+    return (
+      <>
+        <div
+          className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventIncome}`}
+        >
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div
+          className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventExpense}`}
+        >
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div
+          className={`${styles.calendarContainer__money} ${styles.calendarContainer__eventBalance}`}
+        >
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </>
+    );
+  };
+
   return (
     <FullCalendar
       locale={jaLocale}
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      events={events}
+      events={calenderEvents}
       eventContent={renderEventCount}
     />
   );
